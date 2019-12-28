d187c9619c30f98ef6c7b2ce371d24e8c65cd404
samosaboy <varshney.nikunj@gmail.com>
2019-12-27 21:38:32 -0500

    Initial Commit

---
+++ fitness-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitness-app
+//
+//  Created by Hackintoshi9 on 2019-12-27.
+//  Copyright © 2019 sam0sab0y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
